199baca358a9a65b6f0679d253a4e494798d90ef
debasis@ukko.ag <debasis@ukko.ag>
2021-03-11 21:27:39 -0500

    Final push

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
+
 st.write('Trigonmetry Functions')
 function = st.radio('Chose a function to plot', ['Sin', 'Cos'])
 x = []
