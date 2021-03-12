8a0c8865a982e4821f694c0f2edd7b12914f48d0
debasis@ukko.ag <debasis@ukko.ag>
2021-03-11 21:16:24 -0500

    Final push

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
+st.write('Trigonmetry Functions')
+function = st.radio('Chose a function to plot', ['Sin', 'Cos'])
+x = []
+y = []
+pi = 22/7
+if function == 'Sin':
+    for i in np.linspace(0, 720):
+        x.append(np.sin(i*pi/180))
+        y.append(i)
+elif function == 'Cos':
+    for i in np.linspace(0, 720):
+        x.append(np.cos(i*pi/180))
+        y.append(i)
+fig, ax = plt.subplots()
+ax.plot(y,x)
+st.pyplot(fig)
